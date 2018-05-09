63af5cfb63e05bc8748b17815abc99a18c1601c4
Agustin Perez <aperezcristina@gmail.com>
2018-05-09 10:45:26 -0300

    Add config to use v2 api.

--- lib/bigcommerce/config.rb
+++ lib/bigcommerce/config.rb
@@ -8,6 +8,7 @@ module Bigcommerce
       return url if auth == 'legacy'
 
       base = ENV['BC_API_ENDPOINT'].to_s.empty? ? DEFAULTS[:base_url] : ENV['BC_API_ENDPOINT']
+      return "#{base}/stores/#{store_hash}/v2" if v2.present? && v2 == true
       "#{base}/stores/#{store_hash}/v3"
     end
   end
